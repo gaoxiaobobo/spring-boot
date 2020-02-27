a19351b28fcda3ab185dbca26e10a08b2e43cfbc
gaoxiaobo <gaoxiaobo@qq.com>
2020-02-27 08:46:24 +0800

    add jrebel Slf4j annotation

--- spring-boot-helloworld/src/main/java/com/gaoxiaobo/helloworld/controller/HelloWorldController.java
+++ spring-boot-helloworld/src/main/java/com/gaoxiaobo/helloworld/controller/HelloWorldController.java
@@ -1,12 +1,37 @@
 package com.gaoxiaobo.helloworld.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@PropertySource({"classpath:test.properties"})
 public class HelloWorldController {
+    @Value("${user.age:11}")
+    private int age;
+
+    @Value("${user.name2:xx}")
+    private String  name;
+
+    public int getAge(){
+
+        log.warn("age:{}", age);
+        return age;
+    }
+
+    /**
+     *
+     * @return
+     */
     @RequestMapping("/hello")
     public String index(){
-        return "Hello World";
+        log.error("name4:{}",name);
+        getAge();
+        return name;
     }
 }
